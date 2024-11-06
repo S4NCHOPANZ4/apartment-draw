f3c3ebe778280602b659406799809365052ecb12
Juan Buitrago <buitr4go@gmail.com>
2024-11-05 20:30:14 -0500

    router-dom added and tailwind

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HomePage } from "./Routes";
+
+
 function App() {
   return (
-    <>
-     
-    </>
+    <div  className='bg-stone-700 min-h-[100vh]'>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
